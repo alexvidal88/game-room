fc102264cd48c67ed950a817720f9d10df103a3c
Alex Vidal <alexr.vidalh@gmail.com>
2021-09-01 15:50:04 +1000

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+
+  def home
+  end
+
+  def dice
+  end
+
+  def drum
+  end
+
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
-
+import 'bootstrap';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+import { drums } from "../games/hit_the_drums.js";
+import { dice } from "../games/roll_the_dice.js";
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener('turbolinks:load', () => {
+  drums();
+  dice();
+});
